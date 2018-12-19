8f4d7ec5a64e3d20d31c9cd7bf7e29fff295ea90
Lukas van Schagen <lukas.vanschagen@eu.equinix.com>
2018-12-19 09:26:18 +0100

    rewrite to use safer check_output and have python do filtering rather than bash

--- volume-change.py
+++ volume-change.py
@@ -8,49 +8,73 @@
 #
 # Requires: python3 and python-dbus (on Arch) or python3-dbus
 #           (on Debian) or equivalent
-import dbus
-import sys
-from subprocess import getoutput
+from dbus import Interface, SessionBus
+from re import findall, search
+from sys import argv, exit
+from subprocess import check_output
+
+# Set any amixer options we need. Adjust if the default doesn't work for you.
+# base_cmd = ['amixer', '-D', 'pulse']
+# base_cmd = ['amixer', '-D', 'alsa']
+base_cmd = ['amixer']
+volume_type = 'Master'
+
+# Interpreting how to affect the volume and by what percentage
+vol_action = argv[1]
+if vol_action in ['increase', 'decrease']:
+    vol_percent_change = int(argv[2])
+elif vol_action in ['mute']:
+    vol_percent_change = 0
+else:
+    print("ERROR: command not one of 'decrease', 'increase' or 'mute': {}".format(vol_action))
+    exit(1)
 
 # Getting the dbus interface to communicate with gnome's OSD
-session_bus = dbus.SessionBus()
+session_bus = SessionBus()
 proxy = session_bus.get_object('org.gnome.Shell', '/org/gnome/Shell')
-interface = dbus.Interface(proxy, 'org.gnome.Shell')
+interface = Interface(proxy, 'org.gnome.Shell')
 
-# Interpreting how to affect the volume and by what percentage and
-# then creating a bash command that will reduce the stdout to the
-# new percentage volume. Vol = volume
-vol_action = sys.argv[1]
-vol_percent_change = sys.argv[2]
+# Get the current state.
+output = check_output(base_cmd + ['sget', volume_type]).decode('utf-8')
 
-command = 'amixer -D pulse sset Master ' + vol_percent_change + '%'
+# Mute status.
+try:
+    on = search(r'\[(on)?(off)?\]', output).group(1)
+except IndexError:
+    print("ERROR: no volume information found, try changing base_cmd")
+    exit(1)
 
+# Calculate the new level based on an average for all levels.
+vol_percentage = [int(x) for x in findall(r'\[(\d{1,2})\%\]', output)]
+vol_percentage = int(sum(vol_percentage)/len(vol_percentage)+0.5)
 if vol_action == 'increase':
-    command += '+ > /dev/null && amixer -D pulse set Master unmute'
+    vol_percentage += vol_percent_change
+elif vol_action == 'decrease':
+    vol_percentage -= vol_percent_change
+label = "{}%".format(vol_percentage)
+
+# Make actual changes.
+if vol_action in ['increase', 'decrease']:
+    check_output(base_cmd + ['sset', volume_type, "{}%".format(vol_percentage)])
+if on:
+    if vol_action in ['mute'] or vol_action in ['decrease'] and vol_percentage == 0:
+        check_output(base_cmd + ['sset', volume_type, "mute"])
+        label = 'muted'
 else:
-    command += '-'
-
-command += ' | grep -oP "\[\d*%\]" | head -n 1 | sed s:[][%]::g'
-
-current_vol_percentage = int(getoutput(command))
-# If it's 0 then add mute flag (tigger sub-action, keyboard ligth for example)
-if current_vol_percentage == 0:
-    getoutput('amixer -D pulse set Master mute');
+    if vol_action in ['mute', 'increase']:
+        check_output(base_cmd + ['sset', volume_type, "unmute"])
 
 # Determining which logo to use based off of the percentage
 logo = 'audio-volume-'
-label = "{}%".format(current_vol_percentage)
-if current_vol_percentage == 0:
+if vol_percentage == 0:
     logo += 'muted'
-    label = 'muted'
-elif current_vol_percentage < 30:
+elif vol_percentage < 30:
     logo += 'low'
-elif current_vol_percentage < 70:
+elif vol_percentage < 70:
     logo += 'medium'
 else:
     logo += 'high'
 logo += '-symbolic'
 
 # Make the dbus method call
-interface.ShowOSD({"icon":logo, "level":current_vol_percentage, "label": "Master Volume: {}".format(label)})
-
+interface.ShowOSD({"icon": logo, "level": vol_percentage, "label": "{} Volume: {}".format(volume_type, label)})
